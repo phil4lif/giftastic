2252d20d8135fe4f2f347ae0105f40ed11d41c0d
phil cowan <philipjcowan1@gmail.com>
2020-01-12 03:02:38 -0700

    pseudocoding some javascript

---
+++ assets/script/app.js
@@ -0,0 +1,46 @@
+//make an array of starting buttons
+
+
+
+
+//document ready function
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    //write a fumction to make new buttons for the animal inputs
+
+
+
+
+    //write a click function for the animal buttons
+
+
+
+    //ajax call
+    //store my API key in a variable
+    //make a query variable
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
