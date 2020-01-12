8064d38f9cff02b110e4961bae8fbbc45e07de56
phil cowan <philipjcowan1@gmail.com>
2020-01-12 03:26:21 -0700

    gets render button function working to add the initial array of animals

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,5 +1,5 @@
 //make an array of starting buttons
-
+var animalButtons = ["wolf", "elephant", "dog", "sloth", "jaguar",]
 
 
 
@@ -8,6 +8,23 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
     //write a fumction to make new buttons for the animal inputs
+    function renderButtons() {
+        $("#buttons-view").empty();
+
+        // Looping through the array of animals
+        for (var i = 0; i < animalButtons.length; i++) {
+          var a = $("<button>");
+          // Adding a class of animal to our button
+          a.addClass("animal");
+          // Adding a data-attribute
+          a.attr("data-name", animalButtons[i]);
+          // Providing the initial button text
+          a.text(animalButtons[i]);
+          // Adding the button to the buttons-view div
+          $("#animalbuttons").append(a);
+        }
+      }
+      renderButtons()
 
 
 
