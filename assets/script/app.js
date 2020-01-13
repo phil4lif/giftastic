15cfd55de46322d961580818cb87f9a9d72f8b5a
phil cowan <philipjcowan1@gmail.com>
2020-01-13 10:22:49 -0700

    adds ajax call

--- assets/script/app.js
+++ assets/script/app.js
@@ -4,28 +4,27 @@ var animalButtons = ["wolf", "elephant", "dog", "sloth", "jaguar",]
 
 
 //document ready function
-$( document ).ready(function() {
-    console.log( "ready!" );
+$(document).ready(function () {
+    console.log("ready!");
 
-    //write a fumction to make new buttons for the animal inputs
+    //write a fumction to create buttons named for each item in the animalButtons array
     function renderButtons() {
         $("#buttons-view").empty();
-
         // Looping through the array of animals
         for (var i = 0; i < animalButtons.length; i++) {
-          var a = $("<button>");
-          // Adding a class of animal to our button
-          a.addClass("animal");
-          // Adding a data-attribute
-          a.attr("data-name", animalButtons[i]);
-          // Providing the initial button text
-          a.text(animalButtons[i]);
-          // Adding the button to the buttons-view div
-          $("#animalbuttons").append(a);
+            var a = $("<button>");
+            // Adding a class of animal to our button
+            a.addClass("animal");
+            // Adding a data-attribute
+            a.attr("data-name", animalButtons[i]);
+            // Providing the initial button text
+            a.text(animalButtons[i]);
+            // Adding the button to the buttons-view div
+            $("#animalbuttons").append(a);
         }
-      }
-      renderButtons()
-
+    }
+    //invoke the renderButtons function to display the 
+    renderButtons()
 
 
 
@@ -33,11 +32,18 @@ $( document ).ready(function() {
 
 
 
-    //ajax call
-    //store my API key in a variable
-    //make a query variable
     
-
+    //
+    //make a query variable, store my API key in a variable
+    var apiKey ;
+    var queryURL ;
+    //ajax call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response){
+        console.log(response)
+    })
 
 
 
