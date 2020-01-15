292fa178a62eb9b6c050218b846dbe4944344eb7
phil cowan <philipjcowan1@gmail.com>
2020-01-15 12:39:00 -0700

    rewriting some functionality, making a display gifs function to hold the code, rather than in an onclick, using .click listener so that it adds the functionality to the new buttons

--- assets/script/app.js
+++ assets/script/app.js
@@ -6,79 +6,104 @@ var animalButtons = ["wolf", "elephant", "dog", "sloth", "jaguar",]
 //document ready function
 $(document).ready(function () {
     console.log("ready!");
+    //write a function to display the gifs
+    function displayGifs() {
+        var animal = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=WVMqS6EHSAlJ5cuOAPbH2Gw4XKYJtLpz&q=" + animal + "&limit=10&offset=0&rating=G&lang=en";
 
-    //write a fumction to create buttons named for each item in the animalButtons array
-    function renderButtons() {
-        $("#buttons-view").empty();
-        // Looping through the array of animals
-        for (var i = 0; i < animalButtons.length; i++) {
-            var a = $("<button>");
-            // Adding a class of animal to our button
-            a.addClass("animal");
-            // Adding a data-attribute
-            a.attr("data-name", animalButtons[i]);
-            // Providing the initial button text
-            a.text(animalButtons[i]);
-            // Adding the button to the buttons-view div
-            $("#animalbuttons").append(a);
-        }
+        //ajax call
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            // console.log(response)
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div>");
+                var animalImage = $("<img>");
+                animalImage.attr("src", results[i].images.fixed_height.url);
+                gifDiv.prepend(animalImage);
+
+                $("#gifdisplay").prepend(gifDiv);
+
+            }
+        })
+
+    };
+
+
+
+//write a fumction to create buttons named for each item in the animalButtons array
+function renderButtons() {
+    $("#animalbuttons").empty();
+    // Looping through the array of animals
+    for (var i = 0; i < animalButtons.length; i++) {
+        var a = $("<button>");
+        // Adding a class of animal to our button
+        a.addClass("animal");
+        // Adding a data-attribute
+        a.attr("data-name", animalButtons[i]);
+        // Providing the initial button text
+        a.text(animalButtons[i]);
+        // Adding the button to the buttons-view div
+        $("#animalbuttons").append(a);
     }
-    //invoke the renderButtons function to display the beginning array of buttons
-    renderButtons()
+}
+//invoke the renderButtons function to display the beginning array of buttons
+renderButtons()
 
 
 
-    //write a click function for the animal buttons
-$("button").on("click", function(){
-    var animal = $(this).attr("data-name");
-    // console.log($(this).attr("data-name"))
-    console.log(animal)
+//write a click function for the animal buttons
+// $("button").on("click", function () {
+//     var animal = $(this).attr("data-name");
+//     // console.log($(this).attr("data-name"))
+//     console.log(animal)
 
 
-    
+
     //
     //make a query variable
-    
-    var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=WVMqS6EHSAlJ5cuOAPbH2Gw4XKYJtLpz&q="+animal+"&limit=10&offset=0&rating=G&lang=en";
+
+    // var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=WVMqS6EHSAlJ5cuOAPbH2Gw4XKYJtLpz&q=" + animal + "&limit=10&offset=0&rating=G&lang=en";
 
     //ajax call
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response){
-        console.log(response)
-        var results = response.data;
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
+//         // console.log(response)
+//         var results = response.data;
 
-        for(var i = 0; i <results.length; i++){
-            var gifDiv = $("<div>");
-            var animalImage = $("<img>");
-            animalImage.attr("src", results[i].images.fixed_height.url);
-            gifDiv.prepend(animalImage);
+//         for (var i = 0; i < results.length; i++) {
+//             var gifDiv = $("<div>");
+//             var animalImage = $("<img>");
+//             animalImage.attr("src", results[i].images.fixed_height.url);
+//             gifDiv.prepend(animalImage);
 
-            $("#gifdisplay").prepend(gifDiv);
+//             $("#gifdisplay").prepend(gifDiv);
 
-        }
-    })
+//         }
+//     })
 
-});
-
-
+// });
 
 
+//write a function to take the value of the input and add it to the page
+//on click for the input button
+$("#submit").on("click", function (event) {
+    event.preventDefault();
 
+    //grab the inut from the textbox and store it in a new variable
+    var newAnimal = $("#input").val().trim();
+    //add the new animal inpput to the animalButtons array
+    animalButtons.push(newAnimal);
+    console.log(animalButtons);
+    renderButtons();
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).on("click", ".animal", displayGifs);
+//call the renderbuttons func to scan the array and add the new animal
+    renderButtons();
 });
