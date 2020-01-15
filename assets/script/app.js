808188fa743385899efbf3f3c85a6d021110bde7
phil cowan <philipjcowan1@gmail.com>
2020-01-15 12:00:25 -0700

    writes the onclick function for the animal buttons, inserts the new animal var into queryURL and successfully prepends the results to the div on the page

--- assets/script/app.js
+++ assets/script/app.js
@@ -23,20 +23,23 @@ $(document).ready(function () {
             $("#animalbuttons").append(a);
         }
     }
-    //invoke the renderButtons function to display the 
+    //invoke the renderButtons function to display the beginning array of buttons
     renderButtons()
 
 
 
     //write a click function for the animal buttons
-
+$("button").on("click", function(){
+    var animal = $(this).attr("data-name");
+    // console.log($(this).attr("data-name"))
+    console.log(animal)
 
 
     
     //
-    //make a query variable, store my API key in a variable
+    //make a query variable
     
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=WVMqS6EHSAlJ5cuOAPbH2Gw4XKYJtLpz&q=sloth&limit=10&offset=0&rating=G&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?&api_key=WVMqS6EHSAlJ5cuOAPbH2Gw4XKYJtLpz&q="+animal+"&limit=10&offset=0&rating=G&lang=en";
 
     //ajax call
     $.ajax({
@@ -44,9 +47,20 @@ $(document).ready(function () {
         method: "GET"
     }).then(function (response){
         console.log(response)
+        var results = response.data;
+
+        for(var i = 0; i <results.length; i++){
+            var gifDiv = $("<div>");
+            var animalImage = $("<img>");
+            animalImage.attr("src", results[i].images.fixed_height.url);
+            gifDiv.prepend(animalImage);
+
+            $("#gifdisplay").prepend(gifDiv);
+
+        }
     })
 
-
+});
 
 
 
